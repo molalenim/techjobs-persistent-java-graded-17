b7f0d83582e36a479861bb92ab819b673d9620f3
molalenim <molaleni.mabaso@gmail.com>
2024-06-14 19:12:52 -0400

    TASK 3: SETTING UP A ONE-TO-MANY RELATIONSHIP

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +22,14 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+    @Autowired
+    private JobRepository jobRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -27,19 +40,31 @@ public class HomeController {
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
-	model.addAttribute("title", "Add Job");
+        model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId) {
+                                    Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
-	    model.addAttribute("title", "Add Job");
+            model.addAttribute("title", "Add Job");
+            model.addAttribute("employers", employerRepository.findAll());
             return "add";
         }
+
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = optEmployer.get();
+            newJob.setEmployer(employer);
+        } else {
+            return "redirect:/jobs/add?error=EmployerNotFound";
+        }
+
+        jobRepository.save(newJob);
 
         return "redirect:";
     }
@@ -47,7 +72,19 @@ public class HomeController {
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
 
-            return "view";
-    }
+        // Fetch the job from the repository using the jobId
+        Optional<Job> optionalJob = jobRepository.findById(jobId);
 
+        // Check if the job exists
+        if (optionalJob.isPresent()) {
+            // If job exists, add it to the model to be used in the view template
+            Job job = optionalJob.get();
+            model.addAttribute("job", job);
+            return "view"; // Return the view template name
+        } else {
+            // If job does not exist, redirect to a suitable location, e.g., job listing page
+            return "redirect:/jobs";
+        }
+
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,15 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
+
     @NotBlank(message = "Location is required")
     @Size(min = 3, max = 100, message = "Location must be between 3 to a 100 characters")
     private String location;
+
+    @JoinColumn(name = "employer_id")
+    @OneToMany(mappedBy = "employer")
+    private List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
@@ -18,10 +28,6 @@ public class Employer extends AbstractEntity {
     public void setLocation(String location) {
         this.location = location;
     }
-
-/*    public Employer(String location) {
-        this.location = location;
-    }*/
 
     public Employer() {
     }
